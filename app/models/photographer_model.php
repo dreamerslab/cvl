b8a0222a8b76d1a0e1881128ab76a816ba9d8be0
ben <ben@dreamerslab.com>
2012-02-08 15:32:15 +0800

    minor change

--- app/models/photographer_model.php
+++ app/models/photographer_model.php
@@ -1,17 +1,17 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class Photographer_Model extends CI_Model{
-  
+
   private $_data;
-  
+
   public function __construct()
   {
     parent::__construct();
-    
+
     $photographers = array(
       'jack-wang', 'loki-tsai', 'minshi-jiang'
     );
-    
+
     foreach ($photographers as $photographer) {
       $data_path = APPPATH."models/photographer/{$photographer}.yml";
       $this->_data[$photographer] = @file_exists($data_path) ?

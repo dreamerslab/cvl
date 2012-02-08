296026d3d762a789474f84cd3022806520a24972
ben <ben@dreamerslab.com>
2012-02-08 15:31:30 +0800

    rename photographer nav id, class name to gallery for common use

--- app/views/photographer/_nav.php
+++ app/views/photographer/_nav.php
@@ -1,16 +1,20 @@
-<div id="photographer-nav">
-  <div id="photographer-nav-pages">
-    
+<div id="gallery-nav">
+  <div id="gallery-nav-pages">
+
     <?for($i = 1; $i <= $pages; $i++):?>
-    <a class="photographer-nav-item <?selected($i, $page)?>" href="<?=site_url($url_prefix.$i)?>">
+    <a class="gallery-nav-item <?selected($i, $page)?>" href="<?=site_url($url_prefix.$i)?>">
       <?=$i?>
     </a>
     <?endfor?>
-    
+
   </div>
-  <div id="portfolio-type">
-    <a id="homme" class="<?selected('homme', $type)?>" href="<?portfolio_type($nav_selected, 'homme', $page)?>">HOMME</a> 
+  <div id="gender-type">
+    <a id="homme" class="<?selected('homme', $type)?>" href="<?=site_url("/photographer/{$nav_selected}/homme/{$page}")?>">
+      HOMME
+    </a>
     |
-    <a id="femme" class="<?selected('femme', $type)?>" href="<?portfolio_type($nav_selected, 'femme', $page)?>">FEMME</a>
+    <a id="femme" class="<?selected('femme', $type)?>" href="<?=site_url("/photographer/{$nav_selected}/femme/{$page}")?>">
+      FEMME
+    </a>
   </div>
 </div>

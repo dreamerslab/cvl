0b805024f32d78a1c41dd2aae5e531c079225735
ben <ben@dreamerslab.com>
2012-02-08 15:31:59 +0800

    add artists controller

--- app/controllers/artists.php
+++ app/controllers/artists.php
@@ -5,15 +5,28 @@ class Artists extends MY_Controller {
   public function __construct()
   {
     parent::__construct();
+
+    $this->load->model('Artist_Model');
   }
-  
-  public function index()
+
+  public function index($type, $page)
   {
     // add class 'selected' to navigation menu
     $data['nav_selected'] = 'artists';
     $data['contact_url']  = 'contact';
     $data['contact_text'] = 'Contact us';
-    $data['from']         = '?from=artists';
+    $data['artists']      = $this->Artist_Model->all();
+    $data['type']         = $type;
+    $data['page']         = $page;
+    $data['pages']        = count($data['artists'][$type]);
+
+    if( $data['page'] > $data['pages']) $page = $data['page'] = $data['pages'];
+
+    $data['img_prefix'] = "/img/artists/{$type}/{$page}/";
+    $data['url_prefix'] = "/artists/{$type}/";
+    $data['from']       = "?from=artists/{$type}/{$page}";
+
     $this->view->render($data);
+
   }
 }

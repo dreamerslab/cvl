35bd4a51d95cb917cf85a0c9dc7f36b3e1c9ae20
ben <ben@dreamerslab.com>
2012-02-08 15:33:12 +0800

    add artists index view

---
+++ app/views/artists/index.php
@@ -0,0 +1,10 @@
+<? $prefix = $artists[$type][$page];?>
+
+<div id="gallery">
+  <h2 id="gallery-title"><?=$title.' '.$type?></h2>
+  <p>Add something about artists here</p>
+  <img id="first-gallery-img" src="<?=site_url($img_prefix.$prefix[0]['url'])?>" alt="<?=$prefix[0]['desc']?>">
+  <img src="<?=site_url($img_prefix.$prefix[1]['url'])?>" alt="<?=$prefix[1]['desc']?>">
+  <img id="last-gallery-img" src="<?=site_url($img_prefix.$prefix[2]['url'])?>" alt="<?=$prefix[2]['desc']?>">
+  <?$this->view->partial('artists/_nav')?>
+</div>

20fafa666e188002a22cb5816ba62ea8c225aceb
ben <ben@dreamerslab.com>
2012-02-08 15:36:45 +0800

    add modify fashion controller to add gallery nav

--- app/controllers/fashion.php
+++ app/controllers/fashion.php
@@ -6,14 +6,23 @@ class Fashion extends MY_Controller {
   {
     parent::__construct();
   }
-  
-  public function index()
+
+  public function index($page)
   {
     // add class 'selected' to navigation menu
     $data['nav_selected'] = 'fashion';
     $data['contact_url']  = 'contact';
     $data['contact_text'] = 'Contact us';
-    $data['from']         = '?from=fashion';
+    $data['artists']      = $this->Fashion_Model->all();
+    $data['page']         = $page;
+    $data['pages']        = count($data['artists']);
+
+    if( $data['page'] > $data['pages']) $page = $data['page'] = $data['pages'];
+
+    $data['img_prefix'] = "/img/artists/{$page}/";
+    $data['url_prefix'] = "/artists/";
+    $data['from']       = "?from=fashion/{$page}";
+
     $this->view->render($data);
   }
 }
